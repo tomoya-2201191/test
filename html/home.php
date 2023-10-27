c05bd6de4266bcbc2e42fdc13fa5cb0cae643243
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2023-10-27 12:17:10 +0900

    Update home.php

--- html/home.php
+++ html/home.php
@@ -1,37 +1,62 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>ASO CLOTHES</title>
-    <link rel="stylesheet" href="../css/frame.css">
+    <link rel="stylesheet" href="css/frame.css">
+    <title>home</title>
+</head>
+  <style>
+    .parent ul{
+      display: none;
+    }
+    .active {
+      /*background-color: lightyellow;*/
+    }
+    .active ul {
+      display: block;
+    }
+  </style>
 </head>
 <body>
     <header class="header">
-        <a href="home.html">
-            <img src="../img/header.JPG">
+        <a href="home.php">
+            <img src="./img/header.JPG">
         </a>
         <div class="login">
             <a href="login.php">ログイン</a>
+            
         </div>
     </header>
-    <div class="shopping-cart">
-        <a href="#">買い物カゴ</a>
-    </div>
-    <div class="name">
-        <p><u>ホーム</u></p>
+        <div class="shopping-cart">
+            <a href="cart-show.php">買い物カゴ</a>
+        </div>
+    <div class="name"></div>
+            <u><p>ホーム</p></u>
     </div>
     <div class="container">
         <div class="left-menu">
             <ul>
                 <li><a href="#">ホーム</a></li>
-                <li><a href="#">ランキング</a></li>
+                <li class="parent" onclick="func1(this)">ランキング
+                    <ul>
+                      <li><a href="">アウター</a></li>
+                      <li><a href="">トップス</a></li>
+                      <li><a href="">ボトムス</a></li>
+                      <li><a href="">インナー</a></li>
+                      <li><a href="">小物</a></li>
+                    </ul>
+                  </li>
                 <li><a href="#">ユーザー情報更新</a></li>
-                <li><a href="#">商品検索</a></li>
+                <li><a href="productSearch.php">商品検索</a></li>
             </ul>
         </div>
         <div class="main">
-
-        </div>
-    </div>
+          
+    
+  
+  <script>
+    function func1(e) {
+      e.classList.toggle("active");
+    }
+  </script>
 </body>
 </html>
