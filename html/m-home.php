66d33fc9e09cae0eab5e138cddc22c4ecee52f4a
Him-0 <2201194@s.asojuku.ac.jp>
2023-11-02 10:16:28 +0900

    Create m-home.php

---
+++ html/m-home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="css/frame.css">
+    <title>m-home</title>
+</head>
+  
+</head>
+<body>
+  <?php require 'header.php'; ?>
+
+        <div class="main">
+          <!--検索選択リンク-->
+          <a class="circle" href="#">商品登録</a>
+          <a class="circle" href="#">商品削除</a>
+          <a class="circle" href="#">更新</a>
+</div>
+</body>
+</html>
