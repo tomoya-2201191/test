983b95c09f202431d345faa6450948093e8613d6
megumi0303 <2201179@s.asojuku.ac.jp>
2023-11-06 10:20:33 +0900

    Create productSearch.php

---
+++ html/productSearch.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="css/frame.css">
+    <title>home</title>
+</head>
+  
+</head>
+<body>
+  <?php require 'header.php'; ?>
+
+        <div class="main">
+          <!--検索選択リンク-->
+          <a class="p_name" href="product.php">商品名</a>
+          <a class="p_category" href="#">カテゴリ</a>
+          <a class="p_price" href="#">価格</a>
+</div>
+</body>
+</html>
