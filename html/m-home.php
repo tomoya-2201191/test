341a51b099343f3753421ba0cfabe8a2cda0fcc0
Him-0 <2201194@s.asojuku.ac.jp>
2023-11-06 10:48:35 +0900

    Update m-home.php

--- html/m-home.php
+++ html/m-home.php
@@ -1,19 +1,56 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <link rel="stylesheet" href="css/frame.css">
+    <link rel="stylesheet" href="../css/frame.css">
     <title>m-home</title>
 </head>
-  
+  <style>
+    .parent ul{
+      display: none;
+    }
+    .active {
+      /*background-color: lightyellow;*/
+    }
+    .active ul {
+      display: block;
+    }
+  </style>
 </head>
 <body>
-  <?php require 'home.php'; ?>
-
+    <header class="header">
+        <a href="home.php">
+            <img src="../img/header.JPG">
+        </a>
+        <div class="login">
+            <a href="#">ログアウト</a>
+            
+        </div>
+    </header>
+    <div class="name"></div>
+            <u><p>管理者画面</p></u>
+    </div>
+    <div class="container">
+        <div class="left-menu">
+            <ul>
+                <li><a href="#">ホーム</a></li>
+                    <ul>
+                      <li><a href="">商品登録</a></li>
+                      <li><a href="">商品削除</a></li>
+                      <li><a href="">更新</a></li>
+                    </ul>
+                  </li>
+            </ul>
+        </div>
         <div class="main">
-          <!--検索選択リンク-->
-          <a class="circle" href="#">商品登録</a>
-          <a class="circle" href="#">商品削除</a>
-          <a class="circle" href="#">更新</a>
-</div>
+          <a class="p_name" href="#">商品登録</a>
+          <a class="p_category" href="#">商品削除</a>
+          <a class="p_price" href="#">更新</a>
+    
+  
+  <script>
+    function func1(e) {
+      e.classList.toggle("active");
+    }
+  </script>
 </body>
 </html>
