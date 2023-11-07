f565d836716e0545b2b5d2dc961bad933bef93d1
NishiSouta <147477896+NishiSouta@users.noreply.github.com>
2023-11-07 14:00:24 +0900

    Update

---
+++ html/userInUp-output.php
@@ -0,0 +1,75 @@
+<?php require 'dbconnect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" href="../css/frame.css">
+    <title>ユーザー情報更新完了</title>
+</head>
+  <style>
+    .parent ul{
+      display: none;
+    }
+    .active {
+      /*background-color: lightyellow;*/
+    }
+    .active ul {
+      display: block;
+    }
+  </style>
+</head>
+<body>
+    <header class="header">
+        <a href="home.php">
+            <img src="../img/header.JPG">
+        </a>
+        <div class="login">
+            <a href="login.php">ログイン</a>
+            
+        </div>
+    </header>
+        <div class="shopping-cart">
+            <a href="cart-show.php">買い物カゴ</a>
+        </div>
+    <div class="name"></div>ユーザー情報更新完了
+            <u><p></p></u>
+    </div>
+    <div class="container">
+        <div class="left-menu">
+            <ul>
+                <li><a href="#">ホーム</a></li>
+                <li class="parent" onclick="func1(this)">ランキング
+                    <ul>
+                      <li><a href="">アウター</a></li>
+                      <li><a href="">トップス</a></li>
+                      <li><a href="">ボトムス</a></li>
+                      <li><a href="">インナー</a></li>
+                      <li><a href="">小物</a></li>
+                    </ul>
+                  </li>
+                <li><a href="#">ユーザー情報更新</a></li>
+                <li><a href="productSearch.php">商品検索</a></li>
+            </ul>
+        </div>
+        <div class = "main">
+          <?php
+          $pdo = new PDO('mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1516821-asoclothes;charset=utf8',
+          'LAA1516821','Pass0726');
+          $sql=$pdo->prepare('update product set name=?,price=? where id=?'); 
+          if ($sql->execute(
+            $_POST['name'],$_POST['price'],$_POST['id']
+          )) {
+            echo 'ユーザー情報を更新しました。'
+          } else {
+            echo '更新に失敗しました。';
+          }
+        <form class="forms-sample">
+          <h1>ユーザー情報を更新しました。</h1>
+        <button type="submit">ホームへ戻る</button>
+        </form>
+        <script>
+    function func1(e) {
+      e.classList.toggle("active");
+    }
+  </script>
+</body>
+</html>
