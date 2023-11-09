92c90a52b30a3fc6cf2ee867a19729d0fefca646
Him-0 <2201194@s.asojuku.ac.jp>
2023-11-09 11:01:35 +0900

    Update p-insert-input.php

--- html/p-insert-input.php
+++ html/p-insert-input.php
@@ -46,7 +46,8 @@
         <div class="main">
             <form action="p-insert-output.php" method="post">
                 商品名：<input type="text" name="name" style="width: 300px; height=30px"><br>
-                カテゴリ:<input type="text" name="category" style="width: 200px; height=30px"><br>
+                カテゴリ：<input type="text" name="category" style="width: 200px; height=30px"><br>
+                サイズ：<input type="text" name="size" style="width: 200px; height=30px"><br>
                 価格：<input type="text" name="price" style="width: 200px; height=30px"><br>
                 概要：<textarea name="outline" cols="50" rows="3"></textarea><br>
                 在庫：<input type="text" name="stock" style="width: 50px; height=30px">
