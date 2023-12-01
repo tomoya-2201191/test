6fa1c321ad1d474f7ae153f07b505dfa44668261
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2023-12-01 11:01:54 +0900

    Update p-update-output.php

--- html/p-update-output.php
+++ html/p-update-output.php
@@ -69,7 +69,8 @@
             ?>
             <br><hr><br>
             <table>
-                <tr><th>商品ID</th><th>商品名</th><th>カテゴリ</th><th>サイズ</th><th>価格</th><th>概要</th><th>在庫数</th><th>売上数</th></tr>
+                <tr><th>商品ID</th><th>商品名</th><th>カテゴリ</th><th></th><th>サイズ</th><th>価格</th><th>概要</th><th>在庫数</th><th>売上数</th></tr>
+                <tr></tr>
                 <?php
                      $pdo=new PDO($connect, USER, PASS);
                         $sql=$pdo->prepare('select * from product where id=?');
