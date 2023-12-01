e39ecdb60ee7fd4263c7bd498a3a31d93c1a51b7
megumi0303 <2201179@s.asojuku.ac.jp>
2023-12-01 12:12:57 +0900

    UPDATE

--- html/price.php
+++ html/price.php
@@ -25,12 +25,14 @@
             <input type="submit" class="button" value="検索">
         </form>
         </div>
-        <?php
-    echo '<table>';
+    <?php
     $pdo=new PDO($connect, USER, PASS);
     if (isset($_POST['keyword1'], $_POST['keyword2'])) {
         $sql=$pdo->prepare('select * from product where price >= ? and price <= ?');
         $sql->execute([$_POST['keyword1'], $_POST['keyword2']]);
+        $count=$sql -> rowCount();
+        if($count > 0){
+            echo '<table>';
         foreach ($sql as $row) {
             $id=$row['id'];
             echo '<tr>';
@@ -46,6 +48,10 @@
             echo '</tr>';
         }
         echo '</table>';
+        }else{
+            echo '<h2>データが1件もありません</h2>';
+            echo '</table>';
+        }
     }
     ?>
 </body>
