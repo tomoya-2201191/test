5c4c57a9c626a4161d05c31b3b7cb197efad8c32
megumi0303 <2201179@s.asojuku.ac.jp>
2023-12-05 10:42:15 +0900

    Update master-login.php

--- html/master-login.php
+++ html/master-login.php
@@ -117,7 +117,7 @@
                 <div class="form-label">
                     <label for="pass">パスワード：</label>
                 </div>
-                <input type="password" class="txt" name="pass">
+                <input type="password" class="txt" name="password">
             </div>
             <br>
             <div class="login">
