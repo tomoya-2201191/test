4a4b9ad99c21d4f455b8576aea7c4eac3d06a4e9
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2023-12-07 11:14:40 +0900

    Update cart-show.php

--- html/cart-show.php
+++ html/cart-show.php
@@ -165,12 +165,12 @@ if(!empty($_SESSION['product'])){
     echo '<br>';
     echo '<table>';
     echo '<form action="home.php" method="post">';
-    echo '<tr><td><input type="submit" id="button5" value="買い物を続ける"></td>';
+    echo '<tr><td><input type="submit" class="button5" value="買い物を続ける"></td>';
     echo '</form>';
     // 在庫がない商品がある場合購入ボタンを消す
     if(!isset($zerostock)){
     echo '<form action="paymentInformation.php" method="post">';
-    echo '<td><input type="submit" id="button4" value="購入へ進む"></td></tr>';
+    echo '<td><input type="submit" class="button4" value="購入へ進む"></td></tr>';
     echo '</form>';
     }
     echo '</table>';
